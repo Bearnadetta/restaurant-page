cb5cb66275ab8e4ce70a055235658e90d0794f34
Bearnadetta <bearnadetta@gmail.com>
2023-03-03 10:19:04 -0800

    added template for mainDiv and footer, will replace with modules

--- src/functions/pageLoad.js
+++ src/functions/pageLoad.js
@@ -8,10 +8,14 @@ const pageLoad = () => {
     header.textContent = 'Hello'
 
     // creates main content div
-
+    const mainDiv = document.createElement('div')
+    content.appendChild(mainDiv)
+    mainDiv.textContent = 'World'
     // creates footer
+    const footer = document.createElement('div')
+    content.appendChild(footer)
+    footer.textContent = '!'
 
-    
 }
 
 export {pageLoad};
