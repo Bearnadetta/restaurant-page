0f69af1f782c4f804baf2dfe5ac2bc9689679ef9
Bearnadetta <bearnadetta@gmail.com>
2023-03-03 10:15:01 -0800

    bundled pageLoad js file and started writing out behavior for page load

--- src/functions/pageLoad.js
+++ src/functions/pageLoad.js
@@ -1,1 +1,17 @@
-console.log("you bundled it correctly!")
+const pageLoad = () => {
+    console.log("you bundled it correctly!")
+    const content = document.getElementById('content')
+
+    // creates header div
+    const header = document.createElement('div')
+    content.appendChild(header)
+    header.textContent = 'Hello'
+
+    // creates main content div
+
+    // creates footer
+
+    
+}
+
+export {pageLoad};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './style.css';
-
+import { pageLoad } from './functions/pageLoad';
 console.log("It's working!")
+pageLoad();
