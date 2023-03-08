bb89aaf91f1a8cb24657b9b08ca326478d0c5279
Bearnadetta <bearnadetta@gmail.com>
2023-03-08 10:58:44 -0800

    writing out basic functions for DOM manipulation of main div

--- src/functions/mainDiv.js
+++ src/functions/mainDiv.js
@@ -1,7 +1,30 @@
 const mainDiv = () => {
     const main = document.createElement('div')
-    main.textContent = 'World'
     main.classList.add('main')
+
+    const mainHome = () => {
+        const homeDiv = document.createElement('div')
+        homeDiv.classList.add('home-div')
+        homeDiv.textContent = 'Home'
+        main.appendChild(homeDiv)
+        return homeDiv
+    }
+
+    const mainMenu = () => {
+        const menuDiv = document.createElement('div')
+        menuDiv.classList.add('menu-div')
+        menuDiv.textContent = 'Menu'
+        main.appendChild(menuDiv)
+        return menuDiv
+    }
+
+    const mainContact = () => {
+        const contactDiv = document.createElement('div')
+        contactDiv.classList.add('contact-div')
+        contactDiv.textContent = 'Contact'
+        main.appendChild(contactDiv)
+        return contactDiv
+    }
 
     return main
 
